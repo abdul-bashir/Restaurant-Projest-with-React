0478e05814933abe245ea89ff740014e4e3962ba
Abdul Bashir <basir20basir@gmail.com>
2022-07-25 12:19:04 +0600

    Component Tree

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
-
+import MainComponent from "./components/MainComponent";
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <MainComponent/>
+  </div>;
 }
 
 export default App;
