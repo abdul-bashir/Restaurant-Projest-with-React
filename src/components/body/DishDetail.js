d34dcbe0028108a51f9ef5a734cd1ed46bf51d2e
Abdul Bashir <basir20basir@gmail.com>
2022-08-02 19:35:51 +0600

    before debug

--- src/components/body/DishDetail.js
+++ src/components/body/DishDetail.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card, CardImg, CardBody, CardTitle, CardText } from "reactstrap";
-
+import LoadComments from "./LoadComments";
+import dateFormat from "dateformat"; 
 const DishDetail = (props) => {
   return (
     <div>
@@ -8,12 +9,16 @@ const DishDetail = (props) => {
         <CardImg top src={props.dish.image} alt={props.dish.name} />
         <CardBody style={{ textAlign: "left" }}>
           <CardTitle>{props.dish.name}</CardTitle>
-          <CardText><p>{props.dish.description}</p>
-          <p>{props.dish.price}/-</p>
-          
+          <CardText>
+            {props.dish.description}
+            </CardText>
+            <CardText>
+            {props.dish.price}/-
           </CardText>
         </CardBody>
       </Card>
+      <hr />
+      <LoadComments comments={props.dish.comments} />
     </div>
   );
 };
